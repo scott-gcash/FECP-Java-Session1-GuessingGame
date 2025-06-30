e6e14b3cdf2e59d8bf1c0316eee65276f525a2c4
scott-gcash <nicholasscott.campos@gcash.com>
2025-06-30 16:18:26 +0800

    Final Submission, Lab 3: Guessing Game

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# FECP-Java-Session1-GuessingGame
+Name: Nicholas Scott Campos
+
+Project: Lab 3: Guessing Game
+
+Screenshot:
+
+![img.png](img.png)
+
+![img_1.png](img_1.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int secretNumber = (int) (Math.random() * 5) + 1;
+        int guess;
 
+        System.out.println("I'm thinking of a number between 1 and 5");
+        for (int i = 1; i < 4; i++) {
+            System.out.printf("Guess %d: ", i);
+            guess = scanner.nextInt();
+
+            if (guess == secretNumber){
+                System.out.println("You guessed it!");
+                System.out.println("You win!");
+                break;
+            }
+            else{
+                if (i < 3)
+                    System.out.println("Wrong guess.");
+                else
+                    System.out.printf("You lose. Correct number was %d", secretNumber);
+            }
+        }
     }
 }
